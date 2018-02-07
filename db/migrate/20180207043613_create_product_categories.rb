0d5e21d91e058e70e8e40aa82ffa64696cdacb37
addriv <adrivero89@gmail.com>
2018-02-06 20:39:12 -0800

    Generate models and migrations for products, categories, product_categories

---
+++ db/migrate/20180207043613_create_product_categories.rb
@@ -0,0 +1,11 @@
+class CreateProductCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :product_categories do |t|
+      t.integer :product_id, null: false
+      t.integer :category_id, null: false
+      t.timestamps
+    end
+
+    add_index :product_categories, [:product_id, :category_id], unique: true
+  end
+end
