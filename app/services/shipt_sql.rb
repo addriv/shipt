c40098b122de7687269426769734d3954ed81968
addriv <adrivero89@gmail.com>
2018-02-06 20:28:55 -0800

    Write SQL query draft

--- app/services/shipt_sql.rb
+++ app/services/shipt_sql.rb
@@ -1,26 +1,34 @@
 class ShiptSQL
-  
-end
-
-def top_gun_cast
-  MovieDatabase.execute(<<-SQL)
-    SELECT
-      name
-    FROM
-      actors a
-    INNER JOIN
-      castings c
-    ON
-      c.actor_id = a.id
-    INNER JOIN
-      movies b
-    ON
-      b.id = c.movie_id
-    WHERE
-      b.title = 'Top Gun'
-    ORDER BY
-      name
-  SQL
+  def customer_category_purchases
+    "
+      SELECT
+        cs.id AS customer_id,
+        cs.first_name AS customer_first_name,
+        ct.category_id,
+        ct.name AS category_name,
+        SUM(op.order_count) AS number_purchased
+      FROM
+        customers cs
+      INNER JOIN
+        orders o
+      ON 
+        cs.id = o.customer_id
+      INNER JOIN
+        order_products op
+      ON 
+        o.id = op.order_id
+      INNER JOIN
+        product_categories pc
+      ON 
+        op.product_id = pc.product_id
+      INNER JOIN
+        categories ct
+      ON 
+        pc.category_id = ct.id
+      GROUP BY
+        cs.id, cs.first_name, ct.category_id, ct.name
+    "
+  end
 end
 
 # customer_id customer_first_name category_id category_name number_purchased
